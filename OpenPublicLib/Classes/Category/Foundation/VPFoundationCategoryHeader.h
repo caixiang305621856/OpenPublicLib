54cf1e92844e741023c79cca46bd7388abd3855d
caixiang <caix@mail.open.cn>
2018-05-24 10:41:07 +0800

    设置头文件

---
+++ OpenPublicLib/Classes/Category/Foundation/VPFoundationCategoryHeader.h
@@ -0,0 +1,17 @@
+//
+//  VPFoundationCategoryHeader.h
+//  Pods
+//
+//  Created by caixiang on 2018/5/24.
+//
+
+#ifndef VPFoundationCategoryHeader_h
+#define VPFoundationCategoryHeader_h
+#import "NSArray+Additional.h"
+#import "NSDate+Utils.h"
+#import "NSDictionary+Additional.h"
+#import "NSObject+Addtion.h"
+#import "NSString+Additional.h"
+
+
+#endif /* VPFoundationCategoryHeader_h */
---
+++ OpenPublicLib/Classes/Category/UIKit/VPUIKitCategoryHeader.h
@@ -0,0 +1,23 @@
+//
+//  VPUIKitCategoryHeader.h
+//  Pods
+//
+//  Created by caixiang on 2018/5/24.
+//
+
+#ifndef VPUIKitCategoryHeader_h
+#define VPUIKitCategoryHeader_h
+#import "UIImage+Additional.h"
+#import "UIImage+DrawRoundImage.h"
+#import "UIImageView+ShowLarge.h"
+#import "UIImageView+VPImageCache.h"
+#import "UINavigationController+ShouldAutorotate.h"
+#import "UITabBarController+ShouldAutorotate.h"
+#import "UIViewController+ShouldAutorotate.h"
+#import "UITableView+Additional.h"
+#import "UITextField+MaxLength.h"
+#import "UIView+Additional.h"
+#import "UIView+Empty.h"
+
+
+#endif /* VPUIKitCategoryHeader_h */
